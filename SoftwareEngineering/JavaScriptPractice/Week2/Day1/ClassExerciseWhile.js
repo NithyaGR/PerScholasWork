a57951114a8141bbed2e30f4b6538e2855a21e28
Nithyakalyani Govindarajan <nithyakalyanigovindarajan@gmail.com>
2020-12-02 08:51:40 -0500

    MyPractices

---
+++ SoftwareEngineering/JavaScriptPractice/Week2/Day1/ClassExerciseWhile.js
@@ -0,0 +1,8 @@
+var num=0;
+while(num<=20){
+  if(num%2==0){
+    var output=num*3;
+    console.log(output);
+  }
+  num++;
+}
--- SoftwareEngineering/JavaScriptPractice/Week2/Day3/MorningLabHW/filterLongWords.js
+++ SoftwareEngineering/JavaScriptPractice/Week2/Day3/MorningLabHW/filterLongWords.js
@@ -22,3 +22,25 @@ function filterLongWords(stringArray, number){
   var myArray= ["one", "twenty", "some amount","two", "some billions"];
   var myNewArray=filterLongWords(myArray, 5);
   console.log(myNewArray);
+var arr = [];
+console.log(arr.length);
+
+console.log("***************************");
+// let no = 6 // i assigned value sets filter limit in function "word"
+// var arrayInput = ["I","struggle","with","coding","from","time","to","time"]; //created varieable named arrayInput and assigned it words as the input source
+// var arrayCleaned = arrayInput.filter( function( filterLongWords1 ) {
+//   return filterLongWords1.length >= i;
+// });
+
+// Homework 8
+// Write a function filterLongWords that takes an array of words and a number i and returns the array of words that are longer than i characters long.
+  function filterLongWord(complete, i) {
+     let word = [];
+     for(let z=0; z < complete.length; z++){
+         if( complete[z].length > i) {
+            word.push(complete[z]);
+         }
+     }
+     return word;
+ }
+ console.log(filterLongWord(["tennesse", "cameroon", "chelsea", "camouflage"],7))
