399d01e363f75840c356982387676c5ac24f1a22
Nithyakalyani Govindarajan <nithyakalyanigovindarajan@gmail.com>
2020-12-08 22:07:36 -0500

    Updates

--- SoftwareEngineering/TimerAssignments/Exercise4.2.js
+++ SoftwareEngineering/TimerAssignments/Exercise4.2.js
@@ -1,21 +1,25 @@
 window.addEventListener('DOMContentLoaded', documentLoaded);
 
-function documentLoaded() {
-  // document.getElementsByClassName("container")[0].addEventListener("click", clicked);
-  console.log("page document loaded");
-  startTimer(); //to start the startTimer
-}
-
 var startTime; // to calculate the time duration, we need start time.
 var timeLimit; //the time we get from the text box - user's value.
 var temp; //To set the clearInterval
 var currentTime, elapsedTime, minutes, seconds;
 var minutesFormatted, secondsFormatted, formattedTime;
-//This function will get the time interval interval entered from the text box and will
+
+function documentLoaded() {
+  console.log("page document loaded");
+  startTimer(); //to start the startTimer
+  document.getElementsByClassName("container")[0].addEventListener("click", enterTime);
+  //document.getElementsByClassName("editable-in-place")[0].addEventListener("click", clicked);
+  //document.getElementById("textBox").addEventListener("keydown", startTimer, false);
+  //startTimer(); //to start the startTimer
+}
+
+
+//This function will get the time interval entered from the text box and will
 // calculate the start time and the time elapsed
 function startTimer(){
   startTime = new Date().getTime();// instead we can do thisDate = new Date();thisDate.getTime();
-  console.log(timeLimit);
   temp = setInterval(updateTime, 1000); //Learn about this
   }
 
@@ -37,37 +41,38 @@ function updateTime(){
   } else { secondsFormatted = seconds; }
     //displaying the elapsed time
   formattedTime = minutesFormatted+":"+secondsFormatted;
-   document.getElementById("label").innerHTML = formattedTime;
+   document.getElementById("clock").innerHTML = formattedTime;
    console.log("minutes value : "+minutes);
    console.log("time Limit is "+timeLimit);
-   if (minutes >= timeLimit) {
-     document.getElementById("clock").className = "timeIsUp";
-     console.log("Time is Up");
-   } else {
-     document.getElementById("clock").className = "timer";
-     console.log("It's Ticking");
-   }
+   // if (minutes >= timeLimit) {
+   //   document.getElementById("clock").className = "timeIsUp";
+   //   console.log("Time is Up");
+   // } else {
+   //   document.getElementById("clock").className = "timer";
+   //   console.log("It's Ticking");
+   // }
 }
 
 function enterTime(){
   clearInterval(temp);//Stop the startTimer
-
-  document.getElementById("label").contentEditable="true";
-  // document.getElementById("label").innerHTML="";
-  timeLimit = document.getElementById("label").innerHTML;
+  console.log("inside enterTime");
+  document.getElementById("textBox").style.visibility = "visible";
+  console.log("visibility changed to visible");
+  timeLimit = document.getElementById("textBox").value;
   console.log("User entered time limit is : "+timeLimit);
-  label.addEventListener("keydown", function keydown(evt) {
+  textBox.addEventListener("keydown", function keydown(evt) {
 
     // 13 is the code for ENTER
     if (evt.keyCode === 13) {
-      timeLimit = document.getElementById("label").innerHTML;
+      timeLimit = document.getElementById("textBox").innerHTML;
       console.log(timeLimit);
-      document.getElementById("label").contentEditable="false";
+      document.getElementById("textBox").style.visibility = "hidden";
+      console.log("visibility changed to hidden");
       startTimer();
 
       // its important to remove the keydown listener, otherwise in a subsequent edit
       // we will end up with several keydown listeners running
-      label.removeEventListener("keydown", keydown);
+      textBox.removeEventListener("keydown", keydown);
     }
   });
 }
