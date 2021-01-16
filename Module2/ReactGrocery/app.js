14525db7556c1b8182d4ff1aa717ef20416e9e22
Nithyakalyani Govindarajan <nithyakalyanigovindarajan@gmail.com>
2021-01-15 19:55:57 -0500

    GroceryList-Submitted

---
+++ Module2/ReactGrocery/app.js
@@ -0,0 +1,95 @@
+
+class ShoppingList extends React.Component {
+  render() {
+    return(
+      <div className="shoppinglist">
+          <h3> {this.props.list.item}</h3>
+          <h4> {this.props.list.brand}</h4>
+          <h4> {this.props.list.units}</h4>
+          <h4> {this.props.list.quantity}</h4>
+          <input type="checkbox" id="isPurchased" name="isPurchased" />
+          <label htmlFor="isPurchased">Purchased</label>
+      </div>
+    )
+  }
+}
+class App extends React.Component{
+
+  state = {
+      groceries
+  }
+  addToList = (item) => {
+    this.setState({groceries: [item, ...this.state.groceries]})
+  }
+  itemChange = (event) => {
+        this.setState({
+            [event.target.id]: event.target.value
+        })
+    }
+    handlePurchased = (event) => {
+        this.setState({[event.target.id]: event.target.value});
+    }
+  handleSubmit = (event) => {
+      event.preventDefault()
+      console.log("Clicking Submit Button");
+      const newItem = {
+          item: this.state.item,
+          brand: this.state.brand,
+          units: this.state.units,
+          quantity: this.state.quantity,
+          isPurchased: false
+      }
+      console.log(newItem.item);
+      this.setState({
+          groceries :[newItem, ...this.state.groceries],
+          item: '',
+          brand: '',
+          units: '',
+          quantity: '',
+          isPurchased: false
+    })
+  }
+
+  render(){
+    return(
+          <div>
+              <h1> Shopping List</h1>
+              <form onSubmit={this.handleSubmit}>
+                  <label htmlFor="item">Item</label>
+                  <input type="text" value={this.state.item} onChange={this.handleChange} id="item" />
+                  <br />
+                  <label htmlFor="brand">Brand</label>
+                  <input type="text" value={this.state.brand} onChange={this.handleChange} id="brand" />
+                  <br />
+                  <label htmlFor="units">Units</label>
+                  <input type="text" value={this.state.units} onChange={this.handleChange} id="units" />
+                  <br />
+                  <label htmlFor="quantity">Quantity</label>
+                  <input type="number" value={this.state.quantity} onChange={this.handleChange} id="quantity" />
+                  <br />
+                  <input type="submit" />
+              </form>
+          <br />
+          <h3>To Be Purchased</h3>
+          <div className="toBePurchased">
+                      {this.state.groceries.map(list => (<ShoppingList list={list} handleAdd={this.addToList}/>))}
+
+          </div>
+          </div>
+        )
+  }
+
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('.container')
+);
+//handlePurchased={this.handlePurchased}
+
+// {this.state.groceries.map(list => {
+//   return (
+//     <ShoppingList list={list} />
+//   )
+// }
+// )}
---
+++ Module2/ReactGrocery/groceries.js
@@ -0,0 +1,24 @@
+
+const groceries = [
+         {
+          item: 'milk',
+          brand: 'a2',
+          units: '1 gallon',
+          quantity: 2,
+          isPurchased: false
+        },
+        {
+          item: 'candles',
+          brand: 'Stick',
+          units: '1',
+          quantity: 12,
+          isPurchased: false
+        },
+        {
+          item: 'lighter',
+          brand: 'long',
+          units: '1',
+          quantity: 2,
+          isPurchased: false
+        }
+]
